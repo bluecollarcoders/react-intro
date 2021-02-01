c4c8d44387a51ef1fa2b17822d4f3012d678d13e
Caleb Matteis <matteismusic@gmail.com>
2021-02-01 15:17:55 -0500

    added jsx examples along with conditional examples in React

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 const App = () => {
     return (
         <div>
+            
+            <Animal name="Pearl" species="dog" emoji="🐶" isCute={true}/>
+            <Animal name="Chauncy" species="cat" emoji="🐱" isCute={false}/>
             <RandomNum />
             <RandomNum />
             <RandomNum />
+            <Bouncer age={19} />
+            <Bouncer age={11} />
+            <Bouncer age={89} />
+            <Bouncer age={29} />
         </div>
     )
     
